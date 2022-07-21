443cf13fed885b6d3c8a3934e94ebb222975f61b
Dobrosav Vlaskovic <vlaskovic.dodo@gmail.com>
2022-07-21 14:05:55 +0200

    init frontend

---
+++ frontend/src/models/user.ts
@@ -0,0 +1,14 @@
+export class User{
+    ime:string;
+    prezime:string;
+    korime:string;
+    lozinka:string;
+    grad:string;
+    datumRodjenja:string
+    email:string;
+    agencija:string
+    brojLicence:string;
+    telefon:string;
+    tip:number;
+    valid:boolean
+}
